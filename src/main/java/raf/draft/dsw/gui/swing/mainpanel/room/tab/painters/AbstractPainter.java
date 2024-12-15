9a9b285edaf9f37a387b791746334e3c729065af
IvaVidenov <videnov.iva@gmail.com>
2024-12-15 17:11:38 +0100

    Created some painters

--- src/main/java/raf/draft/dsw/gui/swing/mainpanel/room/tab/painters/AbstractPainter.java
+++ src/main/java/raf/draft/dsw/gui/swing/mainpanel/room/tab/painters/AbstractPainter.java
@@ -1,31 +1,38 @@
 package raf.draft.dsw.gui.swing.mainpanel.room.tab.painters;
 
+import raf.draft.dsw.model.structures.room.curves.CircularArc;
+import raf.draft.dsw.model.structures.room.curves.Segment;
+
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 
 public abstract class AbstractPainter{
 
-    protected void drawLine(Point2D a, Point2D b, Graphics2D g2, AffineTransform f){
-        Point2D at = f.transform(a, null);
-        Point2D bt = f.transform(b, null);
-        int x1 = (int)Math.round(at.getX());
-        int y1 = (int)Math.round(at.getY());
-        int x2 = (int)Math.round(bt.getX());
-        int y2 = (int)Math.round(bt.getY());
+    protected void drawLine(Segment s, Graphics2D g2, AffineTransform f){
+        s.transform(f);
+        int x1 = (int)Math.round(s.getA().getX());
+        int y1 = (int)Math.round(s.getA().getY());
+        int x2 = (int)Math.round(s.getB().getX());
+        int y2 = (int)Math.round(s.getB().getY());
         g2.drawLine(x1, y1, x2, y2);
     }
-    protected void drawRectangle(Point2D a, Point2D b, Graphics2D g2, AffineTransform f){
-        Point2D at = f.transform(a, null);
-        Point2D bt = f.transform(b, null);
-        int x1 = (int)Math.round(at.getX());
-        int y1 = (int)Math.round(at.getY());
-        int x2 = (int)Math.round(bt.getX());
-        int y2 = (int)Math.round(bt.getY());
+    protected void drawRectangle(Segment diagonal, Graphics2D g2, AffineTransform f){
+        diagonal.transform(f);
+        int x1 = (int)Math.round(diagonal.getA().getX());
+        int y1 = (int)Math.round(diagonal.getA().getY());
+        int x2 = (int)Math.round(diagonal.getB().getX());
+        int y2 = (int)Math.round(diagonal.getB().getY());
         g2.drawRect(x1, y1, x2-x1, y2-y1);
     }
-    protected void drawArc(){
-
+    protected void drawCircularArc(CircularArc arc, Graphics2D g2, AffineTransform f){
+        arc.transform(f);
+        int x = (int)Math.round(arc.getC().getX() - arc.getR());
+        int y = (int)Math.round(arc.getC().getY() - arc.getR());
+        int a = (int)Math.round(2*arc.getR());
+        int startAngle = (int)Math.round(Math.toDegrees(arc.getStartAngle()));
+        int arcAngle = (int)Math.round(Math.toDegrees(arc.getSweepAngle()));
+        g2.drawArc(x, y, a, a, startAngle, arcAngle);
     }
 
     public abstract void paint(Graphics g, AffineTransform f);
--- src/main/java/raf/draft/dsw/gui/swing/mainpanel/room/tab/painters/WallPainter.java
+++ src/main/java/raf/draft/dsw/gui/swing/mainpanel/room/tab/painters/WallPainter.java
@@ -1,6 +1,7 @@
 package raf.draft.dsw.gui.swing.mainpanel.room.tab.painters;
 
 import lombok.Getter;
+import raf.draft.dsw.model.structures.room.curves.Segment;
 import raf.draft.dsw.model.structures.room.interfaces.Wall;
 
 import java.awt.*;
@@ -23,7 +24,7 @@ public class WallPainter extends AbstractPainter{
         Point2D a = new Point2D.Double(p.getX() + s/2, p.getY() + s/2);
         Point2D b = new Point2D.Double(p.getX() + w - s/2, p.getY() + h - s/2);
         g2.setStroke(new BasicStroke((float)s));
-        drawRectangle(a, b, g2, f);
+        drawRectangle(new Segment(a, b), g2, f);
     }
 
 }
